d76825ba675649c3b155f8d3f8bdf020c31f45f5
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-06-02 21:43:31 -0400

    Adding the HomePage Class of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
+
+const d_green = Color(0xFF54D3C2);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hotels Booking',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Container(),
+    );
+  }
+}
